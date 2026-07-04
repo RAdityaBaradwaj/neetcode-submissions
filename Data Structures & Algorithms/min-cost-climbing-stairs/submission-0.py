1d5eaf080d26fbb9cc1ea973afbd0dff2c6d8e71
Aditya Baradwaj Rajan <72463231+RAdityaBaradwaj@users.noreply.github.com>
2026-07-04 16:05:44 -0400

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [0]*len(cost)
+        if len(cost) <= 2:
+            return min(cost)
+        dp[0],dp[1] = cost[0],cost[1]
+
+        for i in range(2,len(cost)):
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2])
+
+        return min(dp[len(cost)-1],dp[len(cost)-2])
